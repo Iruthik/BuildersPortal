c939b1cbfba24fd6135cb5e45e0449390f565a9e
IRUTHIK SANKAR <74663157+Iruthik@users.noreply.github.com>
2022-02-05 10:43:50 +0530

    worker profile update design compleated

--- use/views.py
+++ use/views.py
@@ -16,10 +16,10 @@ def register(request):
          email = request.POST['email']
          password1 = request.POST['password1']
          password2 = request.POST['password2']
-       
+         role = request.POST['role']
          
 
-         if password1 == password2:
+         if password1 == password2 and role == "1" :
              if User.objects.filter(username =username).exists():
                  messages.info(request,'Username already exist...')
                  return redirect('register')
@@ -32,18 +32,54 @@ def register(request):
              else:
                  user = User.objects.create_user(username=username,password=password1,email=email)
                  user.save()
-                 
+                 print(role)
                  messages.info(request,'user created...')
+                 return redirect('workerupdate')
+       
+                    
+         elif password1 == password2 and role =="2": 
+             if User.objects.filter(username =username).exists():
+                 messages.info(request,'Username already exist...')
+                 return redirect('register')
+
+             elif User.objects.filter(email = email).exists():
+                 messages.info(request,'Email already exist...')
                  return redirect('register')
 
                     
+             else:
+                 user = User.objects.create_user(username=username,password=password1,email=email)
+                 user.save()
+                 print(role)
+                 messages.info(request,'user created...')
+                 return redirect('supplierupdate')
              
+             
+         elif password1 == password2 and role =="3":
+               if User.objects.filter(username =username).exists():
+                 messages.info(request,'Username already exist...')
+                 return redirect('register')
+
+               elif User.objects.filter(email = email).exists():
+                 messages.info(request,'Email already exist...')
+                 return redirect('register')
+
+                    
+               else:
+                 user = User.objects.create_user(username=username,password=password1,email=email)
+                 user.save()
+                 print(role)
+                 messages.info(request,'user created...')
+                 return redirect('customerupdate')
          else:
              messages.info(request,'password not matching!...')
              return redirect('register')
 
-        #  return redirect('register')
-       
+        
+        
+         
+
+
     else:
          return render(request,'use/register.html')   
 
@@ -71,3 +107,13 @@ def logout(request):
 
 def home(request):
     return render(request,'use/home.html')
+
+
+def workerupdate(request):
+    return render(request,'use/worker_update.html')
+
+def supplierupdate(request):
+    return render(request,'use/supplier_update.html')    
+
+def customerupdate(request):
+    return render(request,'use/customer_update.html')
